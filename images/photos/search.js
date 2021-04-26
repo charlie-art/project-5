1b39b54ccdc0429f1d14a60e5849d24fa037a0ef
charlie alton <charlie_alton123@hotmail.com>
2021-04-26 01:10:34 -0600

    update search box

--- images/photos/search.js
+++ images/photos/search.js
@@ -1,16 +1,14 @@
-let searchBox = document.querySelector("#search");
-let pictures = docment.querySelectorAll("a");
+let searchBox = document.querySelector('#search')
+let pictures = docment.querySelectorAll('a');
 
 searchBox.addEventListner("keyup", (event) => {
-
-    if (searchBox.value == null) {
-        console.log(searchBox.value);
-        for (let i = 0; i < pictures.length; i++) {
+    console.log(searchBox.value);
+    for (let i = 0; i < pictures.length; x++) {
+        if (searchBox.value.length == 0) {
             pictures[i].style.display = "inline";
         }
-        if (pictures[i].data.title.includes(searchBox.value()) || searchBox.value.length == 0) {
+        else if (pictures[i].dataset.title.includes(searchBox.value)) {
             pictures[i].style.display = "inline";
-            console.log('showing and value was' + searchBox.value);
         } else {
             pictures[i].style.display = 'none';
         }
